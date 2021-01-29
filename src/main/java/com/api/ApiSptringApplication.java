bda75867a29e7dcce28f7652c61daaf206ca7b70
bryanNNz <bryanlukas1997@gmail.com>
2021-01-29 18:58:00 -0300

    Configuracao Inicial

---
+++ src/main/java/com/api/ApiSptringApplication.java
@@ -0,0 +1,13 @@
+package com.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSptringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSptringApplication.class, args);
+	}
+
+}
